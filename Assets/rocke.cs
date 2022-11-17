0856b10be5e875251c9f679fd18f1268cd34a684
Issa Charbaji <80466126+issacharbaji@users.noreply.github.com>
2022-11-17 12:41:37 +0200

    improve the rocket movement

--- Assets/rocke.cs
+++ Assets/rocke.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class rocke : MonoBehaviour
-{
+{   
+    [SerializeField] float rcsthrust = 100f;
+    [SerializeField] float mainthrust = 10f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -18,31 +23,41 @@ public class rocke : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
-
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainthrust);
             if (!audioSource.isPlaying)
             {
-                audioSource.Play ();
+                audioSource.Play();
             }
-            
+
         }
         else
         {
             audioSource.Stop();
         }
+    } 
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+        
+        float rotationSpeed = rcsthrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationSpeed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationSpeed);
         }
+        rigidBody.freezeRotation = false; //physics control of rotation
     }
+
+   
 }
